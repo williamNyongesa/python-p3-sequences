e0ede2993e71de32e93e8d7a3f3e6e887b9476eb
William Nyongesa <marakawilliam@gmail.com>
2023-09-13 16:53:06 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    sequence = []
+    if length > 0 and length < 2:
+        sequence.append(0)
+    if length > 1  and length < 3:
+        sequence.append(1)
+
+    while len(sequence) < length:
+        next_number = sequence[-1] + sequence[-2]
+        sequence.append(next_number)
+
+    return sequence  # Return the complete sequence
+
+
+result = print_fibonacci(9)
+print(result)
